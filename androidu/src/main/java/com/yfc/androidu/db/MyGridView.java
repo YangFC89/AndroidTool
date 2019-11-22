178f536381be5aea7c803c79bb4f09dc8384f62a
PC-20190517YUSJ\Administrator <617014840@qq.com>
2019-11-22 15:18:05 +0800

    添加城市选择

---
+++ androidu/src/main/java/com/yfc/androidu/db/MyGridView.java
@@ -0,0 +1,22 @@
+package com.yfc.androidu.db;
+
+import android.widget.GridView;
+
+/**
+ * Created by yangfacheng on 2017/3/22.
+ */
+public class MyGridView extends GridView {
+    public MyGridView(android.content.Context context,
+                      android.util.AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    /**
+     * 设置不滚动
+     */
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
+                MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+}
