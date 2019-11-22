d436b68ea668f169752cea64727787471812d80a
PC-20190517YUSJ\Administrator <617014840@qq.com>
2019-11-22 16:34:12 +0800

    删除城市选择

--- app/src/main/java/com/yfc/androidtool/MainActivity.java
+++ app/src/main/java/com/yfc/androidtool/MainActivity.java
@@ -1,7 +1,11 @@
 package com.yfc.androidtool;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.yfc.androidu.db.Activity01;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(MainActivity.this, Activity01.class));
     }
 }
