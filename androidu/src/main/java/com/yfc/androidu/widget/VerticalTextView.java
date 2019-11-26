ee2544d76ecd86494d9a3719b0ec6e422925cc4b
PC-20190517YUSJ\Administrator <617014840@qq.com>
2019-11-26 16:44:27 +0800

    添加垂直滚动控件

---
+++ androidu/src/main/java/com/yfc/androidu/widget/VerticalTextView.java
@@ -0,0 +1,193 @@
+package com.yfc.androidu.widget;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Handler;
+import android.os.Message;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
+
+import java.util.ArrayList;
+
+/**
+ * 垂直滚动的textview
+ */
+public class VerticalTextView extends TextSwitcher implements ViewSwitcher.ViewFactory {
+
+    private static final int FLAG_START_AUTO_SCROLL = 0;
+    private static final int FLAG_STOP_AUTO_SCROLL = 1;
+
+    private static final int STATE_PAUSE = 2;
+    private static final int STATE_SCROLL = 3;
+
+    private float mTextSize = 16;
+    private int mPadding = 5;
+    private int textColor = Color.BLACK;
+
+    private int mScrollState = STATE_PAUSE;
+
+    /**
+     * @param textSize  textsize
+     * @param padding   padding
+     * @param textColor textcolor
+     */
+    public void setText(float textSize, int padding, int textColor) {
+        mTextSize = textSize;
+        mPadding = padding;
+        this.textColor = textColor;
+    }
+
+    private OnItemClickListener itemClickListener;
+    private Context mContext;
+    private int currentId = -1;
+    private ArrayList<String> textList;
+    private Handler handler;
+
+    public VerticalTextView(Context context) {
+        this(context, null);
+        mContext = context;
+    }
+
+    public VerticalTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+        textList = new ArrayList<String>();
+    }
+
+    /**
+     * 设置滚动动画
+     *
+     * @param animDuration
+     */
+    public void setAnimTime(long animDuration) {
+        setFactory(this);
+        Animation in = new TranslateAnimation(0, 0, animDuration, 0);
+        in.setDuration(animDuration);
+        in.setInterpolator(new AccelerateInterpolator());
+        Animation out = new TranslateAnimation(0, 0, 0, -animDuration);
+        out.setDuration(animDuration);
+        out.setInterpolator(new AccelerateInterpolator());
+        setInAnimation(in);
+        setOutAnimation(out);
+    }
+
+    /**
+     * 规定时间
+     *
+     * @param time
+     */
+    @SuppressLint("HandlerLeak")
+    public void setTextStillTime(final long time) {
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case FLAG_START_AUTO_SCROLL:
+                        if (textList.size() > 0) {
+                            currentId++;
+                            setText(textList.get(currentId % textList.size()));
+                        }
+                        handler.sendEmptyMessageDelayed(FLAG_START_AUTO_SCROLL, time);
+                        break;
+                    case FLAG_STOP_AUTO_SCROLL:
+                        handler.removeMessages(FLAG_START_AUTO_SCROLL);
+                        break;
+                }
+            }
+        };
+    }
+
+    /**
+     * 集数据列表
+     *
+     * @param titles
+     */
+    public void setTextList(ArrayList<String> titles) {
+        textList.clear();
+        textList.addAll(titles);
+        currentId = -1;
+    }
+
+    /**
+     * 开始自动滚动
+     */
+    public void startAutoScroll() {
+        mScrollState = STATE_SCROLL;
+        handler.sendEmptyMessage(FLAG_START_AUTO_SCROLL);
+    }
+
+    /**
+     * 停止自动滚动
+     */
+    public void stopAutoScroll() {
+        mScrollState = STATE_PAUSE;
+        handler.sendEmptyMessage(FLAG_STOP_AUTO_SCROLL);
+    }
+
+    @Override
+    public View makeView() {
+        TextView t = new TextView(mContext);
+        t.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
+        t.setMaxLines(1);
+        t.setPadding(mPadding, mPadding, mPadding, mPadding);
+        t.setTextColor(textColor);
+        t.setTextSize(mTextSize);
+
+        t.setClickable(true);
+        t.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (itemClickListener != null && textList.size() > 0 && currentId != -1) {
+                    itemClickListener.onItemClick(currentId % textList.size());
+                }
+            }
+        });
+        return t;
+    }
+
+    /**
+     * 设置onclick侦听器
+     *
+     * @param itemClickListener listener
+     */
+    public void setOnItemClickListener(OnItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    /**
+     * 项点击监听器
+     */
+    public interface OnItemClickListener {
+        /**
+         * callback
+         *
+         * @param position position
+         */
+        void onItemClick(int position);
+    }
+
+    public boolean isScroll() {
+        return mScrollState == STATE_SCROLL;
+    }
+
+    public boolean isPause() {
+        return mScrollState == STATE_PAUSE;
+    }
+
+    //memory leancks.内存
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        if (handler != null) {
+            handler.removeCallbacksAndMessages(null);
+        }
+    }
+}
